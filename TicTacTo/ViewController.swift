a71a7d8f27f2f758aee63b79acb13c5434330bad
SUP'Internet 13 <supinternet13@macbookair13.local>
2017-10-25 10:40:47 +0200

    Initial Commit

---
+++ TicTacTo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TicTacTo
+//
+//  Created by SUP'Internet 13 on 25/10/2017.
+//  Copyright © 2017 SUP'Internet 13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TicTacToTests/TicTacToTests.swift
@@ -0,0 +1,36 @@
+//
+//  TicTacToTests.swift
+//  TicTacToTests
+//
+//  Created by SUP'Internet 13 on 25/10/2017.
+//  Copyright © 2017 SUP'Internet 13. All rights reserved.
+//
+
+import XCTest
+@testable import TicTacTo
+
+class TicTacToTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
